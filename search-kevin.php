78313850fd642169abee2a5d9aaf88a93421d515
Ryan Reynolds <ryanrey@uga.edu>
2015-03-22 23:32:11 -0400

    Update search-kevin.php

--- search-kevin.php
+++ search-kevin.php
@@ -1,11 +1,16 @@
 <?php
  include 'common.php';
  ?>
- 
+ <style>
+<?php include 'bacon.css'; ?>
+</style>
+
+<link href="bacon.css" type="text/css" rel="stylesheet" />
  <p> Films with <?php echo $first?> <?php echo $last?> and Kevin Bacon<p>
 
     <?php 
     //connect to my imdb database
+
       $username = "root";
       $password = "flaker";
       $row_num = 1;
@@ -13,20 +18,19 @@
 try {
     $conn = new PDO('mysql:host=localhost:3306;dbname=imdb', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
-	 
+     
     $stmt = $conn->prepare('SELECT name, year 
-      FROM movies 
-      JOIN roles ON movies.id=roles.movie_id 
-      JOIN roles r ON movies.id=r.movie_id  
-      JOIN actors ON roles.actor_id = actors.id 
-      JOIN actors a ON r.actor_id = a.id 
-      WHERE actors.first_name = :first 
-      AND actors.last_name =:last
-      AND a.first_name = :kevin
-      AND a.last_name = :bacon
-      AND roles.movie_id=r.movie_id 
-      ORDER BY movies.year desc;');
-		
+    	FROM movies 
+    	JOIN roles ON movies.id=roles.movie_id 
+		JOIN roles r ON movies.id=r.movie_id  
+		JOIN actors ON roles.actor_id = actors.id 
+		JOIN actors a ON r.actor_id = a.id 
+        WHERE actors.first_name = :first 
+        AND actors.last_name =:last
+		AND a.first_name = :kevin
+		AND a.last_name = :bacon
+		AND roles.movie_id=r.movie_id 
+		ORDER BY movies.year desc;');
     //function to add quotes to query
     $stmt->bindParam(':first', $first, PDO::PARAM_STR);
     $stmt->bindParam(':last', $last, PDO::PARAM_STR);
@@ -40,11 +44,14 @@ try {
 }catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
-?>
 
-<?php $r = $stmt->fetch(); $rowNum = htmlspecialchars($r['name']); 	
 ?>
-
+<?php $r = $stmt->fetch(); $rowNum = htmlspecialchars($r['name']); ?>
+<?php if(strlen($rowNum) < 1){
+      echo "Actor not found";
+      goto end;
+    }
+  ?>
 
     <table id="myTable">
       <thead>
@@ -55,14 +62,7 @@ try {
         </tr>
     </thead>
     <tbody>
-	
-	<?php while ($r = $stmt->fetch()): $rowNum = htmlspecialchars($r['name']);   ?>
-    
-     <?php if(strlen($rowNum) < 1){
-      echo "Actor not found";
-      goto end;
-    }
-	?>
+    <?php while ($r = $stmt->fetch()): ?>
        <tr>
           <td> <?php echo $row_num++?> </td>
           <td> <?php echo htmlspecialchars($r['name'])?> </td>
@@ -71,8 +71,10 @@ try {
       <?php endwhile; ?>
     </tbody>
 </table>
-<?php end: ?>
 
+
+<?php end: ?>
 <?php
+
 include 'bottom.html';
 ?>
