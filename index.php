2f9fad0e89b699c71cce8f8c9acdaa40148507fe
D Mays <dmmays@uga.edu>
2015-03-22 21:45:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,31 +6,6 @@
 				<h1>The One Degree of Kevin Bacon</h1>
 				<p>Type in an actor's name to see if he/she was ever in a movie with Kevin Bacon!</p>
 				<p><img src="https://webster.cs.washington.edu/images/kevinbacon/kevin_bacon.jpg" alt="Kevin Bacon" /></p>
-
-				<!-- form to search for every movie by a given actor -->
-				<form action="search-all.php" method="get">
-					<fieldset>
-						<legend>All movies</legend>
-						<div>
-							<input name="firstname" type="text" size="12" placeholder="first name" autofocus="autofocus" /> 
-							<input name="lastname" type="text" size="12" placeholder="last name" /> 
-							<input type="submit" value="go" />
-						</div>
-					</fieldset>
-				</form>
-				<br>
-				<!-- form to search for movies where a given actor was with Kevin Bacon -->
-				<form action="search-kevin.php" method="get">
-					<fieldset>
-						<legend>Movies with Kevin Bacon</legend>
-						<div>
-							<input name="firstname" type="text" size="12" placeholder="first name" /> 
-							<input name="lastname" type="text" size="12" placeholder="last name" /> 
-							<input type="submit" value="go" />
-						</div>
-					</fieldset>
-				</form>
-				<br>
 	</body>
 </html>
 <?php include 'bottom.html'; ?>
