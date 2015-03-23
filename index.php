9e71709ce6199ebd3aff86f9a0d5d51953105990
chongt <tiffue@uga.edu>
2015-03-22 22:36:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <?php include 'top.html'; ?>
 <html>
 	<body>
-			<div id="main">
-				<h1>The One Degree of Kevin Bacon</h1>
-				<p>Type in an actor's name to see if he/she was ever in a movie with Kevin Bacon!</p>
-				<p><img src="https://webster.cs.washington.edu/images/kevinbacon/kevin_bacon.jpg" alt="Kevin Bacon" /></p>
+		<div id="main">
+			<h1>The One Degree of Kevin Bacon</h1>
+			<p>Type in an actor's name to see if he/she was ever in a movie with Kevin Bacon!</p>
+			<p><img src="https://webster.cs.washington.edu/images/kevinbacon/kevin_bacon.jpg" alt="Kevin Bacon" /></p>
 	</body>
 </html>
 <?php include 'bottom.html'; ?>
